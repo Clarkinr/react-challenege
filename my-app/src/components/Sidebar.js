676c5851c0539d254afe645bd5658ac16acc245b
Clarkinr <robclarkin1@gmail.com>
2022-08-24 14:46:49 +0000

    Complete challenge

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import css from "./css/Sidebar.module.css"
+
+function Sidebar () { 
+    return (
+        <div className= {css.sidebar} >
+            <a href="#" target="blank">My Photos</a>
+            <a href="#" target="blank">My Illustrations</a>
+            <a href="#" target="blank">My Paintings</a>
+            </div>
+    )
+}
+export default Sidebar; 
